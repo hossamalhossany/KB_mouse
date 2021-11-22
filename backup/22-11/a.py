5a9179a6fb489790727391f73762f17f212ee1f6
hossam <hossam.2030@gmail.com>
2021-11-22 15:52:53 +0200

    111

---
+++ backup/22-11/a.py
@@ -0,0 +1,29 @@
+import pyautogui
+import pywinauto
+from pywinauto.application import Application
+
+
+def run1():
+    # app = Application().start("C:\Users\1495\Desktop\open_episode\aa")
+    aa = Application().start('C:\Program Files (x86)\Microsoft Office\Office16\excel.exe "C:\\Users\\1495\\Desktop\\open_episode\\aa.xlsx"')
+
+    # screen_size = pyautogui.size()
+    # mouse_position = pyautogui.position()
+
+    # print(str(screen_size.width)+"   "+str(screen_size.height))
+
+    # pyautogui.moveTo(120, 750)
+    # print(pyautogui.position())
+    # pyautogui.click(120, 750)
+    # pyautogui.write('hossam asd22222222')
+    # # pyautogui.write("yyyyyyyyyyyyyyyyyy")
+    # # pyautogui.click()
+    # pyautogui.rightClick()
+    # pyautogui.moveTo(120, 700)
+    # # pyautogui.press('enter')
+
+    # pyautogui.click()
+    # pyautogui.click()
+    # pyautogui.click(120, 700)
+    # pyautogui.click('Close window')
+    # pyautogui.click("Don\'t Save")
---
+++ main.py
@@ -0,0 +1,25 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+import a
+
+
+# def run_new_function(x):
+#     print(x)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    a.run1()
+
+    # print_hi('PyCharm ya hossam')
+    # run_new_function('uuuuuuuuuuuu')
+
+    # See PyCharm help at https://www.jetbrains.com/help/pycharm/
